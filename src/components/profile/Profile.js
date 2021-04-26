a366a565540d4d2dca352c03a535a43afd214dc0
ericus123 <amaniericus@gmail.com>
2021-04-27 00:15:07 +0200

    feat(profile):manage user profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import image from "../../assets/img/about.jpeg";
 import "../scss/styles.scss";
 import { Row, Col, Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from "@fortawesome/fontawesome-free-solid";
 import { DecisionModal } from "../Modals/DecisionModal";
 
 const Profile = () => {
   const [advanced, changeAdvanced] = useState("none");
-
+  const [active, setActive] =  useState("about");
+  const [showPassword, setShowPassword] =  useState(false)
   const [formMode, changeFormMode] = useState(true);
   const setAdvanced = () => {
     if (advanced === "none") {
@@ -27,19 +29,28 @@ const Profile = () => {
     <div className="profile">
       <div className="profile_picture">
         <img src={image} />
-        <br />
-        <span>
-          <button className="update">Update</button>
-          <button className="remove" onClick={removeImage}>
-            Remove
-          </button>
-        </span>
+       <div className="file">   
+         <form>
+         Change photo
+           <input type="file"/>
+         </form>
+         </div> 
       </div>
       <div className="profile_details">
         <br />
 
         <br />
-        <Form>
+        <Row className="prof_nav">
+          <ul>
+            <li className = {active == "about" ? "on" : null} onClick={() => {setActive("about")}}>About</li>
+            <li className = {active == "settings" ? "on" : null} onClick={() => {setActive("settings")}}>Advanced Settings</li>
+          </ul>
+        
+        </Row>
+        <br />
+        <br />
+        <br />
+        {active == "about" ? <Form>
           <Form.Row>
             <Form.Group as={Col}>
               <Form.Label>First Name</Form.Label>
@@ -108,44 +119,75 @@ const Profile = () => {
           >
             Update
           </Button>
-        </Form>
-        <br />
-        <Row className="row_5">
-          <div onClick={setAdvanced}>
-            <FontAwesomeIcon icon={faCog} style={{ fontSize: "larger" }} />
-            &nbsp; Advanced Settings
-          </div>
-        </Row>
-        <br />
-        <Row style={{ display: advanced }} className="row_6">
+        </Form> :
+        <>
+        <div>
+          <h1><p>Password</p></h1>
+          <Link onClick={() => {setShowPassword(!showPassword)}} >Change password</Link>
+         {showPassword ?  <Form >
+          <Form.Row>
+            <Form.Group style={{width:"50%"}} >
+              <Form.Label>New Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="******"
+              />
+            </Form.Group>
+            </Form.Row>
+            <Form.Row>
+            <Form.Group style={{width: "50%"}}>
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="******"
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+          <Form.Group style={{width: "50%"}}>
+          <Button
+            onClick={setFormMode}
+            style={{ float: "right" }}
+            variant="primary"
+          >
+            Change
+          </Button>
+          </Form.Group>
+          </Form.Row>
+          <br/>
+          </Form> : null}
+        </div>
+        <Row  className="row_6">
           <div>
             <h4 style={{ color: "red" }}>Danger Zone</h4>
-            <br />
             <h4>Delete Account</h4>
             <br />
             <p>Deleting your account will:</p>
           </div>
           <ul>
             <li>
-              <span style={{ fontSize: "x-large" }}>&bull;</span>&nbsp;Delete
+              <span style={{ fontSize: "large" }}>&bull;</span>&nbsp;Delete
               your profile, along with your authentication associations.
             </li>
-
             <li>
               {" "}
-              <span style={{ fontSize: "x-large" }}>&bull;</span>&nbsp; Delete
-              any and all content you have, such as articles, comments, your
-              reading list or chat messages.
+             <p> <span style={{ fontSize: "large" }}>&bull;</span>&nbsp;Delete
+              any and all content you have, such as articles, comments, your reading list or chat messages.
+             
+              </p>
             </li>
             <li>
               {" "}
-              <span style={{ fontSize: "x-large" }}>&bull;</span>&nbsp;Allow
+              <span style={{ fontSize: "large" }}>&bull;</span>&nbsp;Allow
               your username to become available to anyone.
             </li>
           </ul>
 
           <div className="delete_acc">Delete Account</div>
+          <br/>
+          <br/>
         </Row>
+        </>}
       </div>
     </div>
   );
