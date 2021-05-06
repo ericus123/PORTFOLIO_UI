ecf7d256348d8ab40065a5b866da3e2537e56d0a
ericus123 <amaniericus@gmail.com>
2021-05-06 23:52:51 +0200

    feat(works):manage works

--- src/components/home/Work.js
+++ src/components/home/Work.js
@@ -1,13 +1,24 @@
-import React from "react";
-import "../scss/styles.scss";
+import React, { useEffect } from "react";
+import "./styles.scss";
+import { useDispatch, useSelector } from "react-redux";
 import work1 from "../../assets/img/work1.jpg";
 import work2 from "../../assets/img/work2.jpg";
 import work3 from "../../assets/img/work3.jpg";
 import work4 from "../../assets/img/work4.jpg";
 import work5 from "../../assets/img/work5.jpg";
 import work6 from "../../assets/img/work6.jpg";
+import { GetWorks } from "../../redux/actions/home/work";
 
 const Work = () => {
+  const dispatch = useDispatch();
+
+  const works = useSelector((state) => state.getWorks.works);
+  const message = useSelector((state) => state.getWorks.message);
+  const error = useSelector((state) => state.getWorks.error);
+  // useEffect(() => {
+  //   dispatch(GetWorks());
+  // });
+
   return (
     <div className="work section" id="work">
       <h2 className="section-title">Work</h2>
